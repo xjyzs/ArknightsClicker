aeda29beefba85858415772abef4559ec69c9495
xjyzs <xjyzs135@gmail.com>
2026-02-07 19:01:02 +0800

    #「新增」 - 增加新版应用图标 - 增加“忽略电池优化”请求，以确保后台服务正常运行
    #「优化」
    - “暂停部署”功能重构为后台服务，仅在明日方舟处于前台时生效
    - 提高暂停部署灵敏度
    - “暂停部署”功能现可自定义待部署区高度，以适配更多设备
    - 更新 Gradle 和相关依赖版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xjyzs.arknightsclicker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         androidResources. localeFilters+= listOf("zh")
